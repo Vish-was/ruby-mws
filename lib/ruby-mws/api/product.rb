f738749e32a9f7f809cc0a2e15899beba10f5f5e
Vishwas Nahar <vshwsnahar3@gmail.com>
2019-11-27 14:39:40 +0530

    Added support for product 2011-10-01 API
    Old request url that we required get_matching_product_for_id

---
+++ lib/ruby-mws/api/product.rb
@@ -0,0 +1,18 @@
+module MWS
+  module API
+
+    class Product < Base
+      def_request :get_matching_product_for_id,
+        :verb => :get,
+        :uri => '/Products/2011-10-01',
+        :version => '2011-10-01',
+        :lists => {
+          :seller_sku => "IdList.Id"
+        },
+        :mods => [
+          lambda {|r| r.products = r.products.product if r.products}
+        ]
+    end
+
+  end
+end
